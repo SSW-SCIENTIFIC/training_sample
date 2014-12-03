2de77ac3c7530361b453426f3e3173c3f7478c6a
S-sword <S-sword@s-sword.net>
2014-12-03 15:44:49 +0900

    commit

--- 06_01.php
+++ 06_01.php
@@ -1,12 +1,12 @@
 <?php
 
-$db = new PDO('pgsql:host=localhost;port=5432;dbname=testdb;', 'postgres', '****');
+	$db = new PDO('pgsql:host=localhost;port=5432;dbname=testdb;', 'testuser', '****');
 
-$prepared = $db->prepare('SELECT id, name FROM score_sheet WHERE math > :math_score;');
-$prepared->bindValue(':math_score', 60, PDO::PARAM_INT);
-$prepared->execute();
+	$prepared = $db->prepare('SELECT id, name FROM score_sheet WHERE math > :math_score;');
+	$prepared->bindValue(':math_score', 60, PDO::PARAM_INT);
+	$prepared->execute();
 
-while($result = $prepared->fetch(PDO::FETCH_ASSOC))
-echo($result['id'].' '.$result['name'].PHP_EOL);
+	while($result = $prepared->fetch(PDO::FETCH_ASSOC))
+		echo($result['id'].' '.$result['name'].PHP_EOL);
 
 ?>
